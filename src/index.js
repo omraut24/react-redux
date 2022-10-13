f2bb1fa0ff6ed41ca3c4144b2b98eaf7bb865209
Omkar Raut <omkarraut2412@gmail.com>
2022-10-13 18:31:23 +0530

    made changes in code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 
 store.subscribe(() => console.log(store.getState()));
 
